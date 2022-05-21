4c053e9db5edd971aec43b191e0ada6e56fd5615
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2022-05-22 03:53:36 +0430

    feat(customers comment): add comments to the section

--- src/components/Customer/customerCarousel.tsx
+++ src/components/Customer/customerCarousel.tsx
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
 import data from "./customerData.json";
+import "./carouselOverride.css";
+import styled from "styled-components";
+import { NONAME } from "dns";
 
-export default function CustomerCarousel() {
-  const customerData = data.customerData.map((customer) => (
-    <div>
+export default function CustomerCarousel({
+  enterComment,
+}: {
+  enterComment: () => void;
+}) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const customerData = data.customerData.map((customer, index) => (
+    <Item
+      key={customer.name}
+      onClick={() => {
+        setCurrentIndex(index);
+      }}
+    >
       <img src={customer.logo} alt={customer.name} />
+    </Item>
+  ));
+
+  const comments = data.customerData.map((item) => (
+    <div>
+      <img src={item.comment.profilePic} alt={item.comment.author} />
+      <h3>{item.comment.author}</h3>
+      <h2>{item.comment.position}</h2>
+      <div>
+        <p>{item.comment.comment}</p>
+        <p>{item.comment.subComment}</p>
+      </div>
     </div>
   ));
   return (
-    <section>
-      <Carousel width={6}>{customerData}</Carousel>
-    </section>
+    <>
+      <CommentContainer>{comments}</CommentContainer>
+      <CarouselSection onClick={enterComment}>{customerData}</CarouselSection>
+    </>
   );
 }
+
+const CarouselSection = styled.section`
+  display: flex;
+  align-items: center;
+  margin-top: 74px;
+`;
+const Item = styled.div`
+  width: 9vw;
+  height: 9vw;
+  border-radius: 50%;
+  background: linear-gradient(180deg, #75c9db 0%, #4af3f8 100%);
+  box-shadow: inset 0px 0px 80px #75c9db80, 0px 3px 3px #8125254d;
+  border: 1px solid #75c9db4d;
+  backdrop-filter: blur(7px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 2vw;
+  cursor: pointer;
+
+  & > img {
+    width: 70%;
+    height: 70%;
+    object-fit: contain;
+  }
+`;
+
+const CommentContainer = styled.div`
+  display: none;
+`;
--- src/components/Customer/index.tsx
+++ src/components/Customer/index.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CustomerCarousel from "./customerCarousel";
+import { useSpring, animated, easings } from "react-spring";
 
 export default function Customer() {
+  const [showComment, setShowComment] = useState(false);
+  const styleProps2 = useSpring({
+    from: { opacity: 1 },
+    to: { opacity: showComment ? 0 : 1 },
+    config: { duration: 1000, easing: easings.easeOutQuart },
+  });
   return (
     <CustomerElement>
-      <div>
-        <p>
+      <Statistics style={styleProps2}>
+        <UntilToday>
           تا امروز
-          <span>1401/03/03</span>
-        </p>
-        <p>سیستم حضور و غیاب تیکمنت</p>
-        <p>
+          <Date>1401/03/03</Date>
+        </UntilToday>
+        <MiddleText>سیستم حضور و غیاب تیکمنت</MiddleText>
+        <TotalCustomer>
           در
-          <span>34561</span>
+          <TotalNumber>34561</TotalNumber>
           سازمان خصوصی و دولتی فعال بوده است.
-        </p>
-      </div>
-      <div>
-        <CustomerCarousel />
-      </div>
+        </TotalCustomer>
+      </Statistics>
+      <CustomerCarousel
+        enterComment={() => {
+          setShowComment(true);
+        }}
+      />
     </CustomerElement>
   );
 }
@@ -31,4 +40,43 @@ const CustomerElement = styled.section`
   flex-direction: column;
   justify-content: center;
   padding: 8vh 10vw;
+`;
+
+const Statistics = styled(animated.div)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UntilToday = styled.p`
+  font-size: 3vw;
+  color: #183573;
+  display: flex;
+  align-items: center;
+`;
+
+const Date = styled.span`
+  display: inline-block;
+  font-size: 5vw;
+  padding: 0 18px;
+  line-height: 128px;
+  background: #f9f8f7 0% 0% no-repeat padding-box;
+  box-shadow: inset 0px 0px 80px #75c9db80, 0px 3px 3px #8125254d;
+  border: 1px solid #75c9db4d;
+  border-radius: 36px;
+  opacity: 1;
+  backdrop-filter: blur(2px);
+  margin-right: 25px;
+`;
+
+const MiddleText = styled.p`
+  font-size: 4vw;
+  color: #e67205;
+  margin: 0;
+`;
+
+const TotalCustomer = styled(UntilToday)``;
+
+const TotalNumber = styled(Date)`
+  margin: 0 25px;
+  color: #3b0002;
 `;
