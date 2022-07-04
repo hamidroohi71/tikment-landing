b0d40f1f8f5cb7b2321237b97ca12f60e136ad28
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2022-07-04 09:11:04 +0430

    fix(product): fix icon button in other sections

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -4,13 +4,16 @@ import ProductSlider from "./ProductSlider";
 import { useSection } from "../../context/sectionStore";
 import { useSpring, animated, easings } from "react-spring";
 import useWidth from "../../hooks/useWidth";
+import TopHandleBack from "./topHandleback.webp";
+import HandleSign from "./handleIcon.png";
 
 let timeOut: NodeJS.Timeout;
 
 export default function Product() {
   const [titleOn, setTitleOn] = useState(true);
   const width = useWidth();
-  const { activeSection, nextSection, setActiveSection } = useSection();
+  const { activeSection, setNextSection, nextSection, setActiveSection } =
+    useSection();
   const [active, setActive] = useState(false);
 
   const titlePartStyle = useSpring({
@@ -66,6 +69,14 @@ export default function Product() {
       active={active}
       status={nextSection === 3 ? "show" : nextSection < 3 ? "before" : "after"}
     >
+      <TopHandle
+        onClick={() => {
+          setNextSection(3);
+          setActiveSection(null);
+        }}
+      >
+        <HandleIcon />
+      </TopHandle>
       <TitlePart style={titlePartStyle}>
         <MainTitle style={titleStyle}>تیکمنت چیست؟</MainTitle>
         <SubTitle style={subTitleStyle}>
@@ -78,6 +89,7 @@ export default function Product() {
       <ProductPart style={ProductStyle}>
         <ProductSlider />
       </ProductPart>
+      <BottomHandle />
     </ProductSection>
   );
 }
@@ -86,7 +98,6 @@ const ProductSection = styled.section<{ status: string; active: boolean }>`
   position: absolute;
   height: 100vh;
   width: 100vw;
-  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -102,7 +113,7 @@ const ProductSection = styled.section<{ status: string; active: boolean }>`
       ? "translateY(100vh)"
       : "translateY(-100vh)"};
   transition: 0.5s ease-in;
-  z-index: ${({ active }) => (active ? 20 : 0)};
+  z-index: 20;
 
   @media (max-width: 480px) {
     position: static;
@@ -112,6 +123,31 @@ const ProductSection = styled.section<{ status: string; active: boolean }>`
     height: 120vh;
   }
 `;
+
+const TopHandle = styled.div`
+  position: absolute;
+  top: -103px;
+  bottom: 100%;
+  left: 6vw;
+  right: 57vw;
+  background: url(${TopHandleBack}) center/contain no-repeat;
+  cursor: pointer;
+`;
+
+const HandleIcon = styled.div`
+  width: 3.5vw;
+  height: 3.5vw;
+  border-radius: 50%;
+  background: url(${HandleSign}) center/contain no-repeat;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+`;
+
+const BottomHandle = styled.div``;
 
 const TitlePart = styled(animated.div)`
   display: flex;
